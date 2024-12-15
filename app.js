86f9569612602bb08d538854a28ab6d53459904a
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-12-15 20:49:23 +0530

    Added About SignUp Button with JS

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 let signUp = document.querySelector(".signup_button");
 signUp.innerHTML = "Sign up here";
 console.log(signUp);
-signUp.appendChild("signUp");
+let banner = document.querySelector(".content");
+banner.appendChild(signUp);
+
+function clickable() {
+  alert("button clicked");
+  let signIn_page = document.createElement("a");
+  signIn_page.innerHTML = "About SignUp";
+  signIn_page.setAttribute("class", "sign");
+  console.log(signIn_page);
+  let content = document.querySelector(".content");
+  content.appendChild(signIn_page);
+}
